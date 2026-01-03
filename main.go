9d051b7721a909b2972e608fca3b04016c785508
WhyIsEmerald <WhyIsEmerald@proton.me>
2026-01-03 14:31:03 +0530

    chore: Restore main.go to a simple state

--- main.go
+++ main.go
@@ -1,17 +1,4 @@
 package main
 
-import (
-	"log"
-
-	"github.com/WhyIsEmerald/GPDB/internals/db"
-)
-
 func main() {
-	db, err := db.NewDB[string, string]()
-	if err != nil {
-		log.Fatalf("Failed to create DB: %v", err)
-	}
-	db.Put("a", "apple")
-	val, _ := db.Get("a")
-	log.Printf("Get('a') = %s (should be 'apple')", val)
 }
